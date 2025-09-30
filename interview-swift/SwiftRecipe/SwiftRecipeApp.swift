0559eb03ba10680570f5648b5b543920045a1600
Faozi Bouybaouene Gadrouz <fbg171@gmail.com>
2025-10-01 00:23:51 +0200

    rever change in order to have a tabview

--- interview-swift/SwiftRecipe/SwiftRecipeApp.swift
+++ interview-swift/SwiftRecipe/SwiftRecipeApp.swift
@@ -3,12 +3,19 @@ import SwiftUI
 
 @main
 struct SwiftRecipeApp: App {
-    var body: some Scene {
-        WindowGroup {
-          RecipeListView()
-            .tabItem {
-              Label("SwiftUI", systemImage: "swift")
-            }
-        }
+  var body: some Scene {
+    WindowGroup {
+      TabView {
+        RecipeListView()
+          .tabItem {
+            Label("SwiftUI", systemImage: "swift")
+          }
+        
+        RecipeListViewControllerBridge()
+          .tabItem {
+            Label("UIKit", systemImage: "square.stack.3d.down.forward")
+          }
+      }
     }
+  }
 }
