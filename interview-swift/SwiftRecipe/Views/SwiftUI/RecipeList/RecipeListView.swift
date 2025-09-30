a29aa560c77e0db19bec52867b7bd721be7ffd5f
Faozi Bouybaouene Gadrouz <fbg171@gmail.com>
2025-09-30 21:44:20 +0200

    Init of list view, debounce search

--- interview-swift/SwiftRecipe/Views/SwiftUI/RecipeList/RecipeListView.swift
+++ interview-swift/SwiftRecipe/Views/SwiftUI/RecipeList/RecipeListView.swift
@@ -4,15 +4,34 @@ import Combine
 import Foundation
 
 struct RecipeListView: View {
-    private var viewModel = RecipeListViewModel()
+  @StateObject private var viewModel = RecipeListViewModel()
 
     var body: some View {
         NavigationView {
             // TODO: - #3: Display a search bar and a list of results.
             //  - perform the search using the view model
             //  - The view should match the image in screenshots/swiftui_search_results.png
-            EmptyView()
-            .navigationTitle("Recipes")
+          
+          Group {
+            if viewModel.state.isLoading {
+              Text("Loading...")
+              ProgressView()
+                .progressViewStyle(.circular)
+            } else if viewModel.recipeSearchQuery.isEmpty {
+              Color.clear
+            } else if let statusMessage = viewModel.state.statusMessage {
+              // no search found --> status message
+            } else {
+              // returned recipes
+            }
+            
+          }
+          .navigationTitle("Recipes")
+          .searchable(text: $viewModel.recipeSearchQuery, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search Recipes")
         }
     }
+}
+
+#Preview {
+  RecipeListView()
 }
--- interview-swift/SwiftRecipe/Views/SwiftUI/RecipeList/RecipeListViewModel.swift
+++ interview-swift/SwiftRecipe/Views/SwiftUI/RecipeList/RecipeListViewModel.swift
@@ -3,35 +3,54 @@ import Foundation
 import SwiftUI
 import Combine
 
-final class RecipeListViewModel {
-
-    private let recipeService: RecipeService
-
-    // TODO: - #2:  In RecipeListViewModel: publish search results to the RecipeListView using the
-    //  RecipeService. The screen should show a blank page when the query is empty, a loading
-    //  indicator while the page is loading, and a list of results when the repository returns data.
-    //  See the screenshots in README.md.
-
-    init(recipeService: RecipeService = RecipeService()) {
-        self.recipeService = recipeService
+final class RecipeListViewModel: ObservableObject {
+  
+  private let recipeService: RecipeService
+  // To keep subscriptions from combine alive, until the user exists.
+  private var cancellables = Set<AnyCancellable>()
+  
+  // TODO: - #2:  In RecipeListViewModel: publish search results to the RecipeListView using the
+  //  RecipeService. The screen should show a blank page when the query is empty, a loading
+  //  indicator while the page is loading, and a list of results when the repository returns data.
+  //  See the screenshots in README.md.
+  
+  @Published var state: State = .default
+  @Published var recipeSearchQuery: String = ""
+  
+  init(recipeService: RecipeService = RecipeService()) {
+    self.recipeService = recipeService
+    
+    // Debounced search
+    $recipeSearchQuery
+      .debounce(for: 0.3, scheduler: DispatchQueue.main)
+      .removeDuplicates()
+      .sink { searchValue in
+        print(searchValue)
+      }
+      .store(in: &cancellables)
+      
+  }
+  
+  struct State {
+    let isLoading: Bool
+    let statusMessage: String?
+    let recipes: [Recipe]
+    
+    init(
+      isLoading: Bool = false,
+      statusMessage: String? = nil,
+      recipes: [Recipe]? = nil
+    ) {
+      self.isLoading = isLoading
+      self.statusMessage = statusMessage
+      self.recipes = recipes ?? []
     }
     
-    struct State {
-        let isLoading: Bool
-        let statusMessage: String?
-        let recipes: [Recipe]
-        
-        init(
-            isLoading: Bool = false,
-            statusMessage: String? = nil,
-            recipes: [Recipe]? = nil
-        ) {
-            self.isLoading = isLoading
-            self.statusMessage = statusMessage
-            self.recipes = recipes ?? []
-        }
-        
-        static let `default` = State()
-        static let loading = State(isLoading: true)
-    }
+    static let `default` = State()
+    static let loading = State(isLoading: true)
+  }
+  
+  func searchRecipes(recipeSearchQuery: String){
+    // search for recipes.
+  }
 }
