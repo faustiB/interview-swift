5172b9a7e04e27ddb07bf635181b1ff16f45cc33
Faozi Bouybaouene Gadrouz <fbg171@gmail.com>
2025-09-30 21:43:36 +0200

    decision on going to swiftUI

--- interview-swift/SwiftRecipe/SwiftRecipeApp.swift
+++ interview-swift/SwiftRecipe/SwiftRecipeApp.swift
@@ -5,18 +5,9 @@ import SwiftUI
 struct SwiftRecipeApp: App {
     var body: some Scene {
         WindowGroup {
-            // TODO: - #1: choose a UI interface (feel free to change the order of the tabs)
-            TabView {
-                RecipeListView()
-                    .tabItem {
-                        Label("SwiftUI", systemImage: "swift")
-                    }
-                
-                RecipeListViewControllerBridge()
-                .tabItem {
-                    Label("UIKit", systemImage: "square.stack.3d.down.forward")
-                }
-                
+          RecipeListView()
+            .tabItem {
+              Label("SwiftUI", systemImage: "swift")
             }
         }
     }
